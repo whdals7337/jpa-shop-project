8da94cd14bccc476379ef30750d0276a1fe005cb
wellbell <uok02018496@naver.com>
2021-01-18 21:09:50 +0900

    주문 도메인 개발 - 주문,주문상품 엔티티, 리포지토리, 서비스, 테스트

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -11,6 +13,7 @@ import java.util.List;
 @Entity
 @Getter @Setter
 @Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
     @Id @GeneratedValue
@@ -38,8 +41,49 @@ public class Order {
         member.getOrders().add(this);
     }
 
+    public void addOrderItem(OrderItem orderItem) {
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    // 생성 메서드
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if(getDelivery().getStatus() == DeliveryStatus.COMP) {
+            throw  new IllegalStateException("이미 배송완료된 상품은 최소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem orderItem : getOrderItems()) {
+            orderItem.cancel();
+        }
+    }
+
+    /**
+     *
+     * @return 총 주문 가격
+     */
+    public int getTotalPrice() {
+        return getOrderItems().stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
     }
 }
